1c74685245d6dec320f733a01dc20a35d7671429
wolfdog27 <kylenicholasanderson@gmail.com>
2020-07-08 23:17:55 -0700

    engineer on appjs

--- app.js
+++ app.js
@@ -10,6 +10,69 @@ const outputPath = path.join(OUTPUT_DIR, "team.html");
 
 const render = require("./lib/htmlRenderer");
 
+const teamMembers = [];
+
+function createTeam() {
+    inquirer.prompt ({
+        type: "list",
+        choices: [
+            "Add Engineer?",
+            "Add Intern?",
+            "Add Manager?",
+            "Create Team Page?"],
+        name: "userChoice",
+        message:"Would you like to...?",
+        
+    }).then(function ({ choice }) {
+        switch (choice) {
+            case "Add Engineer":
+                console.log("You added an Engineer")
+                makeEngineer();
+                break;
+
+            // case "Add Intern":
+            //     console.log("You added an Intern")
+            //     makeIntern();
+            //     break;
+
+        }
+    })
+}
+createTeam();
+
+
+function makeEngineer() {
+    inquirer.prompt([
+        {
+            type: "input",
+            message: "Employee's Name?",
+            name: "name"
+        },
+        {
+            type: "input",
+            message: "Employee's Id?",
+            name: "id"
+        },
+        {
+            type: "input",
+            message: "Employee's E-mail?",
+            name: "email"
+        },
+        {
+            type: "input",
+            message: "Employee's GitHub Name?",
+            name: "github"
+        },
+    ]).then(function (answers) {
+        const newEngineer = new Engineer(answers.name, answers.id, answers.email, answers.github);
+        teamMembers.push(newEngineer);
+    })
+}
+
+
+
+
+
 
 // Write code to use inquirer to gather information about the development team members,
 // and to create objects for each team member (using the correct classes as blueprints!)
